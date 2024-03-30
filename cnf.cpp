b77971b8d22edd8f5e4e71ea975698daf7ac6a50
dragondoodler222 <dashiellb100@gmail.com>
2024-03-30 14:39:19 -0400

    bad commit message

--- cnf.cpp
+++ cnf.cpp
@@ -99,6 +99,99 @@ void gen_cnf(int N) {
     out.close();
 }
 
-int main() {
-    gen_cnf(32);
+void gen_cnf_slope(int N) {
+    ofstream out;
+    out.open("out.cnf");
+    int num_clauses = 0;
+
+    out << "                                        \n";
+    int **IDs = new int*[N];
+    for (int i = 0; i < N; i++) {
+        IDs[i] = new int[N];
+        for (int j = 0; j < N; j++) {
+            IDs[i][j] = i * N + j + 1;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            out << IDs[i][j] << " ";
+        }
+        out << "0\n";
+        num_clauses++;
+
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < j; k++) {
+                out << "-" << IDs[i][j] << " -" << IDs[i][k] << " 0\n";
+                num_clauses++;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            out << IDs[j][i] << " ";
+        }
+        out << "0\n";
+        num_clauses++;
+
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < j; k++) {
+                out << "-" << IDs[j][i] << " -" << IDs[k][i] << " 0\n";
+                num_clauses++;
+            }
+        }
+    }
+
+    int var_index = pow(N, 2) + 1;
+
+    for (int m_x = 1; m_x < N; m_x++) {
+        for (int m_y = 1; m_y < N; m_y++) {
+            if (m_x == N - 1 && m_y == N - 1) continue;
+
+            int additional_vars = 0;
+
+            for (int i = 0; i < N - m_x; i++) {
+                for (int j = 0; j < N - m_y; j++) {
+                    // Point 1: i, j
+                    // Point 2: i + mx, j + my
+                    // -P_(1,2) = -x[i][j] v -x[i + mx][j + my]
+                    if (i == 0 && j == 0) {
+                        out << "-" << IDs[i][j]  << " -" << IDs[i + m_x][j + m_y] << " " << var_index + additional_vars << " 0\n";
+                        additional_vars++;
+                        num_clauses++;
+                        continue;
+                    }
+
+                    if (i == N - m_x - 1 && j == N - m_y - 1) {
+                        out << "-" << IDs[i][j]  << " -" << IDs[i + m_x][j + m_y] << " -" << var_index + additional_vars - 1 << " 0\n";
+                        // additional_vars++;
+                        num_clauses++;
+                        continue;
+                    }
+
+                    out << "-" << var_index + additional_vars - 1 << " " << var_index + additional_vars << " 0\n";
+                    out << "-" << IDs[i][j]  << " -" << IDs[i + m_x][j + m_y] << " " << var_index + additional_vars << " 0\n";
+                    out << "-" << IDs[i][j]  << " -" << IDs[i + m_x][j + m_y] << " -" << var_index + additional_vars - 1 << " 0\n";
+                    num_clauses += 3;
+
+                    additional_vars++;
+
+                }
+            }
+
+            var_index += additional_vars;
+
+        }
+    }
+
+    // int strlen = 6 + to_string(pow(N, 2)).length() + 1 + to_string(num_clauses).length();
+    out.seekp(0);
+    out << "p cnf " << pow(N, 2) << " " << num_clauses;
+    out.close();
+
+}
+
+int main(int argc, char* argv[]) {
+    gen_cnf_slope(stoi(argv[1]));
 }
